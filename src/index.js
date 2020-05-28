7b89d2f7957ee00ac9e363afeb43f0d342276ac5
leejessica <ezh7464@gmail.com>
2020-05-28 10:55:40 -0400

    geolotation componentDidMout

--- src/index.js
+++ src/index.js
@@ -3,26 +3,23 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     constructor (props) {  
-        super(props);      
-        
-        this.state = {lat: null, errorMessage: ' ' }; 
+        super(props);     
+        this.state = { lat: null, errorMessage: ' ' }; 
+    } 
+
+    componentDidMount () {
         window.navigator.geolocation.getCurrentPosition(
-            position => {    
-             this.setState ({ lat: position.coords.latitude});      
-            },   
-            
-            err => {
-              this.setState ({errorMessage: err.message}); 
-            } 
+            position =>  this.setState ({ lat: position.coords.latitude}),    
+            err => this.setState ({errorMessage: err.message})  //call-back funciton
         ); 
-} 
+    }
 
     //React says we have to define render!! as a method 
     //conditional Rendering 
     render() {      
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>;
-        }
+        // if (this.state.errorMessage && !this.state.lat) {
+        //     return <div>Error: {this.state.errorMessage}</div>;
+        // }
 
         if (!this.state.errorMessage && this.state.lat) {
             return <div>Latitude: {this.state.lat}</div>;
